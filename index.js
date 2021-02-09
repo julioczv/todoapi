60e7ac3e87cfd46a86bde09762c295d535d4658f
Julio Azevedo <zjukiita@gmail.com>
2021-02-09 16:08:27 -0300

    Lista De Tarefas

--- index.js
+++ index.js
@@ -3,13 +3,51 @@ const app = express();
 const port = 3000
 const dados = require('./dados.json');
 const bodyParser = require('body-parser')
-const { Pessoa } = require('./models');
-
+const { Tarefa } = require('./models');
 
 
 app.use(bodyParser.json());
 
+app.get('/tarefas', async (req, res) => {
+    const tarefas = await Tarefa.findAll();
+    res.status(200).json(tarefas);
 
+});
+
+app.get('/tarefas/:id', async (req, res) => {
+    const tarefa = await Tarefa.findAll({
+        where: {
+            id: req.params.id
+        }
+
+    });
+    res.status(200).json(tarefa)
+});
+
+app.post('/tarefas', async (req, res) => {
+    const tarefa = await Tarefa.create(req.body)
+    res.status(201).json(tarefa)
+
+});
+
+app.delete('/tarefas', async (req, res) => {
+    const tarefa = await Tarefa.destroy({
+        where: {
+            id: req.params.id
+
+        }
+    });
+    res.status(200).json(tarefa)
+});
+
+app.put('/tarefas/:id', async (req, res) => {
+    const tarefa = await Tarefa.update(req.body, {
+        where: {
+            id: req.params.id
+        }
+    });
+    res.status(200).json(tarefa);
+});
 
 app.listen(port);
 
